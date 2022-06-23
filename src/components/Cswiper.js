f7948d1ac8de59d69904e340eca03e05f6d2da4a
ahmet demir <ahmet.demir@medyasoft.com.tr>
2022-06-24 01:18:46 +0300

    sw

--- src/components/Cswiper.js
+++ src/components/Cswiper.js
@@ -13,7 +13,7 @@ import { useEffect } from 'react';
 export default () => {
 
     function activeIndex(index) {
- 
+        $('.prev').attr('disabled', false);
         if (index === 0) {
             $('.prev').attr('disabled', true);
 
